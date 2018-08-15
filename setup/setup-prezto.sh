7b9f695028684913683f24819dc793b00eb63379
John Donovan <jjdonov@gmail.com>
2018-08-14 22:26:31 -0700

    add setup for prezto

---
+++ setup/setup-prezto.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+git clone --recursive https://github.com/jjdonov/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+chsh -s /bin/zsh
